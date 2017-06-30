b068cec8f623d9e43a5aacb1297a2d9af3288b1e
theseddd <theseddd@gmail.com>
2017-06-30 20:57:00 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,4 @@
-<?php
-curl -X GET \
+curl -X POST \
+-H 'Content-Type:application/json' \
 -H 'Authorization: Bearer {9KexXFutJpVWfiA12ZrAIZunVdn6qH6Vi3mOdVYC9ojtWXSma5jbx14jv9eZebEA0cgEDbSqGYxNsb3NpKpGB+FtCVb8ketT6hmEamLvl9pIyv9UFKDQQkF5N2Zb2e/husUH9dAwX1Yrx4XRm+EuPgdB04t89/1O/w1cDnyilFU=}' \
-https://api.line.me/v1/oauth/verify
-?>
+...
