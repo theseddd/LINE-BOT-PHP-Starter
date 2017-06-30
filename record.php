0f8c80dcbe480ad6d97b95ecaf914c5d20b26f9d
theseddd <theseddd@gmail.com>
2017-07-01 02:47:45 +0700

    Update record.php

--- record.php
+++ record.php
@@ -1,31 +1,38 @@
 <?php
 
-if(isset($_GET['id'])&&isset($_GET['type'])){
+if(isset($_GET['id'])&&isset($_GET['type'])&&isset($_GET['value'])){
   $id = (string)$_GET['id'];
   $type = (string)$_GET['type'];
-  
+  $value = (string)$_GET['value'];
   $api_key="pTxcx5ycWTLaFNILWW59S9eMdSiDHQrz";
   $url = 'https://api.mlab.com/api/1/databases/line_bot/collections/node?apiKey='.$api_key.'';
-  $json = file_get_contents('https://api.mlab.com/api/1/databases/line_bot/collections/node?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
+  $json = file_get_contents('https://api.mlab.com/api/1/databases/line_bot/collections/node?apiKey='.$api_key.'&q={"id":"'.$id.'"}');
   $data = json_decode($json);
   $isData=sizeof($data);
-
-  //Post New Data
-  $newData = json_encode(
-    array(
-      'question' => $id,
-      'answer'=> $type
-    )
-  );
-  $opts = array(
-    'http' => array(
-        'method' => "POST",
-        'header' => "Content-type: application/json",
-        'content' => $newData
-     )
-  );
-  $context = stream_context_create($opts);
-  $returnValue = file_get_contents($url,false,$context);
+  if($isData >0){
+   $uid;
+   foreach($data as $rec){
+    $uid = $rec->_id;
+   }
+   echo $uid;
+  }else{
+    //Post New Data
+    $newData = json_encode(
+      array(
+        'question' => $id,
+        'answer'=> $type
+      )
+    );
+    $opts = array(
+      'http' => array(
+          'method' => "POST",
+          'header' => "Content-type: application/json",
+          'content' => $newData
+       )
+    );
+    $context = stream_context_create($opts);
+    $returnValue = file_get_contents($url,false,$context);  
+  }
   echo "OK";
 }else{
   echo "No data";
