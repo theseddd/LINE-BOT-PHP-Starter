9e704b41369a1b8ac54a291b25c1f52a1bc293a8
theseddd <theseddd@gmail.com>
2017-07-01 12:37:30 +0700

    Update record.php

--- record.php
+++ record.php
@@ -1,12 +1,16 @@
 <?php
-
+date_default_timezone_set("Asia/Bangkok");
 
 
 
 if(isset($_GET['id'])&&isset($_GET['type'])&&isset($_GET['value'])){
+  $time = date("d-m-Y H:i");
   $id = (string)$_GET['id'];
   $type = (string)$_GET['type'];
   $value = (string)$_GET['value'];
+  if(isset($_GET['mode'])){
+    
+  }
   $api_key="pTxcx5ycWTLaFNILWW59S9eMdSiDHQrz";
   $url = 'https://api.mlab.com/api/1/databases/line_bot/collections/node?apiKey='.$api_key.'';
   $json = file_get_contents('https://api.mlab.com/api/1/databases/line_bot/collections/node?apiKey='.$api_key.'&q={"id":"'.$id.'"}');
@@ -14,25 +18,26 @@ if(isset($_GET['id'])&&isset($_GET['type'])&&isset($_GET['value'])){
   $isData=sizeof($data);
   if($isData >0){
    $uid;
-    echo "1.1";
+   // echo "1.1";
    print_r ($data); 
-    echo "1.2";
+    //echo "1.2";
    $array_ = json_decode(json_encode($data[0]),true);
    print_r ($array_);
-    echo "1.3";
+   // echo "1.3";
    $array__ = (string)$array_['_id']['$oid'];
    print_r ($array__);
-    echo "1.4";
+    //echo "1.4";
     //Post New Data
     $newData = json_encode(
       array(
         '_id' => array('$oid' => $array__),
         'id' => $id,
         'type'=> $type,
+        'time' => $time,
         'value' => $value
       )
     );
-    echo "1.5";
+    //echo "1.5";
     $opts = array(
       'http' => array(
           'method' => "POST",
@@ -40,20 +45,21 @@ if(isset($_GET['id'])&&isset($_GET['type'])&&isset($_GET['value'])){
           'content' => $newData
        )
     );
-    echo "1.6";
+    //echo "1.6";
     $context = stream_context_create($opts);
     $returnValue = file_get_contents($url,false,$context);  
   }else{
-    echo "2.1";
+    //echo "2.1";
     //Post New Data
     $newData = json_encode(
       array(
         'id' => $id,
         'type'=> $type,
+        'time' => $time,
         'value' => $value
       )
     );
-    echo "2.2";
+    //echo "2.2";
     $opts = array(
       'http' => array(
           'method' => "POST",
@@ -61,7 +67,7 @@ if(isset($_GET['id'])&&isset($_GET['type'])&&isset($_GET['value'])){
           'content' => $newData
        )
     );
-    echo "2.3";
+    //echo "2.3";
     $context = stream_context_create($opts);
     $returnValue = file_get_contents($url,false,$context);  
   }
