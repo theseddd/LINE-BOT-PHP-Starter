7413218991a875c72ed2496e25085b2d9b4d190f
theseddd <theseddd@gmail.com>
2017-07-01 12:59:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,51 +17,103 @@ $url = 'https://api.mlab.com/api/1/databases/line_bot/collections/line_bot?apiKe
 $json = file_get_contents('https://api.mlab.com/api/1/databases/line_bot/collections/line_bot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
 $data = json_decode($json);
 $isData=sizeof($data);
- 
-if (strpos($_msg, 'สอน') !== false) {
-  if (strpos($_msg, 'สอน') !== false) {
-    $x_tra = str_replace("สอน","", $_msg);
-    $pieces = explode("|", $x_tra);
-    $_question=str_replace("[","",$pieces[0]);
-    $_answer=str_replace("]","",$pieces[1]);
-    //Post New Data
-    $newData = json_encode(
-      array(
-        'question' => $_question,
-        'answer'=> $_answer
-      )
-    );
-    $opts = array(
-      'http' => array(
-          'method' => "POST",
-          'header' => "Content-type: application/json",
-          'content' => $newData
-       )
-    );
-    $context = stream_context_create($opts);
-    $returnValue = file_get_contents($url,false,$context);
-    $arrPostData = array();
-    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = 'ขอบคุณที่สอนนะคะ';
-  }
+
+ $nurl = 'https://api.mlab.com/api/1/databases/line_bot/collections/node?apiKey='.$api_key.'';
+ $njson = file_get_contents('https://api.mlab.com/api/1/databases/line_bot/collections/node?apiKey='.$api_key.'&q={"id":"'.$id.'"}');
+ $ndata = json_decode($njson);
+ $nisData=sizeof($ndata);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if(isset($_GET['bot'])){
+ if(isset($_GET['message'])){
+  $message = $_GET['message'];
+  $chOne = curl_init();
+  curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify");
+  // SSL USE
+  curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0);
+  //POST
+  curl_setopt( $chOne, CURLOPT_POST, 1);
+  // Message
+  curl_setopt( $chOne, CURLOPT_POSTFIELDS, $message);
+  //ถ้าต้องการใส่รุป ให้ใส่ 2 parameter imageThumbnail และimageFullsize
+  //curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=$message&imageThumbnail=http://10.10.10.10/small.jpg&imageFullsize=http://10.10.10.10/large.jpg");
+  // follow redirects
+  curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1);
+  //ADD header array
+  $headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$strAccessToken, );  // หลังคำว่า Bearer ใส่ line authen code ไป
+  curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers);
+  //RETURN
+  curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1);
+  $result = curl_exec( $chOne );
+  //Check error
+  if(curl_error($chOne)) { echo 'error:' . curl_error($chOne); }
+  else { $result_ = json_decode($result, true);
+  echo "status : ".$result_['status']; echo "message : ". $result_['message']; }
+  //Close connect
+  curl_close( $chOne );
+
+
+
+
+
+
+
+ }else{
+
+
+
+
+
+
+
+ }
 }else{
-  if($isData >0){
-   foreach($data as $rec){
-    $arrPostData = array();
-    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = $rec->answer;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+ if (strpos($_msg, 'สอน') !== false) {
+   if (strpos($_msg, 'สอน') !== false) {
+     $x_tra = str_replace("สอน","", $_msg);
+     $pieces = explode("|", $x_tra);
+     $_question=str_replace("[","",$pieces[0]);
+     $_answer=str_replace("]","",$pieces[1]);
+     //Post New Data
+     $newData = json_encode(
+       array(
+         'question' => $_question,
+         'answer'=> $_answer
+       )
+     );
+     $opts = array(
+       'http' => array(
+           'method' => "POST",
+           'header' => "Content-type: application/json",
+           'content' => $newData
+        )
+     );
+     $context = stream_context_create($opts);
+     $returnValue = file_get_contents($url,false,$context);
+     $arrPostData = array();
+     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+     $arrPostData['messages'][0]['type'] = "text";
+     $arrPostData['messages'][0]['text'] = 'ขอบคุณที่สอนนะคะ';
    }
-  }else{
-    $arrPostData = array();
-    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = 'ฉันไม่รู้จักคำนี้ค่ะ!!! คุณสามารถสอนได้เพียงพิมพ์: สอน[คำถาม|คำตอบ]';
-  }
-}
- 
- 
+ }else{
+   
+   if($isData >0){
+    foreach($data as $rec){
+     $arrPostData = array();
+     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+     $arrPostData['messages'][0]['type'] = "text";
+     $arrPostData['messages'][0]['text'] = $rec->answer;
+    }
+   }else{
+     $arrPostData = array();
+     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+     $arrPostData['messages'][0]['type'] = "text";
+     $arrPostData['messages'][0]['text'] = 'ฉันไม่รู้จักคำนี้ค่ะ!!! คุณสามารถสอนได้เพียงพิมพ์: สอน[คำถาม|คำตอบ]';
+   }
+ }
+
+} 
 $channel = curl_init();
 curl_setopt($channel, CURLOPT_URL,$strUrl);
 curl_setopt($channel, CURLOPT_HEADER, false);
