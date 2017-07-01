c03cbc3be9fe9a6c36e22bf86f8109e8dd9f3fd1
theseddd <theseddd@gmail.com>
2017-07-01 17:08:23 +0700

    Update record.php

--- record.php
+++ record.php
@@ -31,9 +31,9 @@ if(isset($_GET['id'])&&isset($_GET['type'])&&isset($_GET['value'])){
     //echo "1.4";
     //Post New Data
 
-    /*if(('work' => $data) == "1"){
+    if($check == "1"){
       $working = "1";
-    }*/
+    }
     
     
     $newData = json_encode(
