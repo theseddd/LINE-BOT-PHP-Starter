b1216c6e8a03e6b54c656452ff39c59c9219c478
theseddd <theseddd@gmail.com>
2017-07-01 12:10:45 +0700

    Update record.php

--- record.php
+++ record.php
@@ -26,7 +26,7 @@ if(isset($_GET['id'])&&isset($_GET['type'])&&isset($_GET['value'])){
     //Post New Data
     $newData = json_encode(
       array(
-        '_id' => $array__,
+        '_id' => '$oid' => $array__,
         'id' => $id,
         'type'=> $type,
         'value' => $value
